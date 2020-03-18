b39390220434cccd3a0ca57ffd79ed11284246a3
Daniel Sasse Ulloa <48774887+dsasseulloa@users.noreply.github.com>
2020-03-17 21:49:29 -0300

    up

--- ListasED/src/com/company/Lista.java
+++ ListasED/src/com/company/Lista.java
@@ -7,7 +7,7 @@ class NoLista {
     public void setInfo(int info){
         this.info=info;
     }
-    public int getInfo(){
+    public Integer getInfo(){
         return this.info;
     }
     public void setProx(NoLista prox){
@@ -95,4 +95,213 @@ public class Lista {
         }
         return ant;
     }
+
+
+
+
+    public void retira(int v) {
+
+        NoLista[] nos = buscaComAnterior(v);
+
+        if (nos == null){
+            System.out.println("Elemento não pertence à lista");
+        } else if (nos[0].getInfo() == null) {
+            this.prim = nos[1].getProx();
+        } else {
+            nos[0].setProx(nos[1].getProx());
+        }
+    }
+
+    public void libera() {
+        this.prim = null;
+    }
+
+    public void insereOrdenado(int v) {
+        NoLista ant = null;
+        NoLista novo;
+        NoLista p = prim;
+
+        while (p != null && (int) p.getInfo() < (int) v) {
+            ant = p;
+            p = p.getProx();
+        }
+
+        novo = new NoLista();
+        novo.setInfo(v);
+
+        if (ant == null) {
+            novo.setProx(prim);
+            prim = novo;
+        } else {
+            novo.setProx(ant.getProx());
+            ant.setProx(novo);
+        }
+    }
+
+    public boolean igual(Lista l) {
+        NoLista p1 = this.prim;
+        NoLista p2 = l.prim;
+
+        while (p1 != null && p2 != null) {
+            if (p1.getInfo() != p2.getInfo()) {
+                return false;
+            }
+            p1 = p1.getProx();
+            p2 = p2.getProx();
+        }
+        return p1 == p2;
+    }
+
+    public void imprimeRecursivo() {
+        imprimeRecursivoAux(prim);
+    }
+
+    private void imprimeRecursivoAux(NoLista p) {
+        if (p != null) {
+            System.out.print(p);
+            imprimeRecursivoAux(p.getProx());
+        }
+    }
+
+    public void retiraRecursivo(Object v) {
+        prim = retiraRecursivoAux(prim, v);
+    }
+
+    private NoLista retiraRecursivoAux(NoLista l, Object v) {
+        if (l != null) {
+            if (l.getInfo() == v) {
+                l = l.getProx();
+            } else {
+                l.setProx(retiraRecursivoAux(l.getProx(), v));
+            }
+        }
+        return l;
+    }
+
+    public boolean igualRecursivo(Lista l) {
+        return igualRecursivoAux(this.prim, l.prim);
+    }
+
+    private boolean igualRecursivoAux(NoLista p1, NoLista p2) {
+
+        if (p1 == null && p2 == null) {
+            return true;
+        }
+        if (p1 == null || p2 == null) {
+            return false;
+        }
+        return (p1.getInfo() == p2.getInfo()
+                && igualRecursivoAux(p1.getProx(), p2.getProx()));
+
+    }
+
+    public NoLista buscaRecursiva(Object v) {
+        return buscaRecursivaAux(prim,v);
+    }
+
+    public NoLista buscaRecursivaAux(NoLista p, Object v){
+        if (p == null) {
+            return null;
+        }
+        if (p.getInfo() == v){
+            return p;
+        }
+        return buscaRecursivaAux(p.getProx(), v);
+    }
+
+    public int comprimentoRecursivo() {
+        NoLista p = prim;
+        return comprimentoRecAux(p, 0);
+    }
+
+    private int comprimentoRecAux(NoLista p, int count) {
+        if (p == null) {
+            return count;
+        }
+        count++;
+        return comprimentoRecAux(p.getProx(), count);
+    }
+
+
+    public NoLista ultimoRecursivo() {
+        return ultimoRecursivoAux(prim, null);
+    }
+
+    private NoLista ultimoRecursivoAux(NoLista p, NoLista ant) {
+        if (p == null) {
+            return ant;
+        }
+        return ultimoRecursivoAux(p.getProx(), p);
+    }
+
+    public NoLista[] buscaComAnterior(int v) {
+        NoLista ant = new NoLista();
+        NoLista p = prim;
+        NoLista[] nos = {ant, p};
+
+        while (p != null) {
+            if (p.getInfo() == v) {
+                return nos;
+            }
+            nos[0] = p;
+            nos[1] = p.getProx();
+            p = nos[1];
+        }
+        return null;
+    }
+
+    public NoLista buscaPorPosicao(int i) {
+        if (i >= comprimento()){
+            return null;
+        }
+        return buscaPorPosicaoAux(i, 0);
+    }
+    public NoLista buscaPorPosicaoAux(int i, int k){
+        if (k > i) {
+            return null;
+        }
+
+        return buscaPorPosicaoAux(i, ++k);
+
+    }
+
+
+    public boolean insereEm(int posicao, int v){
+        NoLista novo;
+        NoLista ult;
+        int tam = comprimento();
+
+        if (posicao >= tam){
+            ult = ultimoRecursivo();
+            for(int i = tam; i < posicao;i++){
+                ult.setProx(new NoLista());
+                ult = ult.getProx();
+            }
+            novo = new NoLista();
+            novo.setInfo(v);
+            novo.setProx(null);
+            ult.setProx(novo);
+            return true;
+        }
+
+        if (posicao >= 0 && posicao < tam){
+            novo = new NoLista();
+            novo.setInfo(v);
+            NoLista[] nos = buscaComAnterior(posicao);
+
+            if(nos == null || nos[0] == null){
+                novo.setProx(prim);
+                prim = novo;
+            } else {
+                novo.setProx(nos[1].getProx());
+                nos[1].setProx(novo);
+
+            }
+            return true;
+        }
+
+        return false;
+    }
 }
+
+
--- ListasED/src/com/company/ListaMain.java
+++ ListasED/src/com/company/ListaMain.java
@@ -4,18 +4,56 @@ public class ListaMain {
     public static void main(String[] args) {
 
         Lista lista = new Lista();
+        Lista lista2 = new Lista();
+        Lista lista3 = new Lista();
         lista.Insere(9);
         lista.Insere(3);
         lista.Insere(2);
         lista.Insere(5);
+        System.out.println("Lista: ");
         lista.Imprime();
-        System.out.print("\nlista tostring: ");
-        System.out.println(lista.toString());
-        System.out.println("Lista vazia? "+lista.vazia());
-        System.out.println("Nó na lista: "+lista.busca(9));
-        System.out.println("Comprimento da lista: "+lista.comprimento()+ " números");
-        System.out.println("Ultimo num da lista: "+lista.ultimo());
+//        System.out.print("\nlista tostring: ");
+//        System.out.println(lista.toString());
+//        System.out.println("Lista vazia? "+lista.vazia());
+//        System.out.println("Nó na lista: "+lista.busca(9));
+//        System.out.println("Comprimento da lista: "+lista.comprimento()+ " números");
+//        System.out.println("Ultimo num da lista: "+lista.ultimo());
+        System.out.println("\nRetira 3:");
+        lista.retira(3);
+        lista.Imprime();
+        lista.libera();
+        System.out.println("\nLibera:");
+        lista.Imprime();
+        System.out.println("vazio");
+        System.out.println("\ninserçao de ordenados: 10,12,11,9");
+        lista.insereOrdenado(10);
+        lista.insereOrdenado(12);
+        lista.insereOrdenado(11);
+        lista.insereOrdenado(9);
+        System.out.println("impressão recursiva:");
+        lista.imprimeRecursivo();
+        System.out.println("\nretirar recursivamente: 9");
+        lista.retiraRecursivo(9);
+        lista.imprimeRecursivo();
 
+        System.out.println("\n\nCriaçao de lista2 e lista3:");
+        System.out.print("\nLista: ");
+        lista.imprimeRecursivo();
+        System.out.print("\n\nLista2: ");
+        lista2.insereOrdenado(10);
+        lista2.insereOrdenado(12);
+        lista2.insereOrdenado(11);
+        lista2.imprimeRecursivo();
+        System.out.println("\nComparaçao de da lista com lista2: ");
+        System.out.println(lista.igualRecursivo(lista2));
+
+        System.out.print("\nLista3: ");
+        lista3.insereOrdenado(10);
+        lista3.insereOrdenado(12);
+        lista3.insereOrdenado(14);
+        lista3.imprimeRecursivo();
+        System.out.println("\nComparaçao de da lista com lista3: ");
+        System.out.println(lista.igualRecursivo(lista3));
 
     }
 }
